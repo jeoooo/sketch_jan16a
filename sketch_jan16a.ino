4084eaf5729f324854f09a79861976070b7094b1
JEO_CARLO LUBAO <jeocarlolubao@gmail.com>
2024-01-16 11:59:49 +0800

    add comments

--- sketch_jan16a.ino
+++ sketch_jan16a.ino
@@ -1,53 +1,90 @@
+/**
+ *
+ * HTTP/HTTPS GET REQUEST
+ *
+ * [REFERENCE CODE]
+ *
+ * HTTP AND HTTPS GET REQUEST FOR ESP8266
+ *
+ * Created Jan 16 2024
+ * By jeoooo
+ *
+ * Modified Jan 16 2024
+ * By jeoooo
+ *
+ */
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <BearSSLHelpers.h>
 
-const char* ssid = "dlink-3CCB";
-const char* password = "rvtbf69439";
+const char *ssid = "dlink-3CCB";
+const char *password = "rvtbf69439";
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
 }
 
-void httpGETRequest(const char* url) {
+/**
+ * Performs an HTTP GET request to the specified URL.
+ *
+ * @param url The URL to send the GET request to.
+ */
+void httpGETRequest(const char *url)
+{
   // create an HTTPClient instance
   HTTPClient http;
 
   // Initializing an HTTP communication
   Serial.print("[HTTP] begin...\n");
-  if (http.begin(url)) {  // HTTP
+  if (http.begin(url))
+  { // HTTP
     Serial.print("[HTTP] GET...\n");
 
     // start connection and send HTTP header
     int httpCode = http.GET();
 
     // httpCode will be negative on error
-    if (httpCode > 0) {
+    if (httpCode > 0)
+    {
       // HTTP header has been sent, and Server response header has been handled
       Serial.printf("[HTTP] GET... code: %d\n", httpCode);
 
       // file found at server
-      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+      {
         String payload = http.getString();
         Serial.println(payload);
       }
-    } else {
+    }
+    else
+    {
       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
     http.end();
-  } else {
+  }
+  else
+  {
     Serial.printf("[HTTP] Unable to connect\n");
   }
 }
 
-void httpsGETRequest(const char* url) {
+/**
+ * Performs an HTTPS GET request to the specified URL.
+ *
+ * @param url The URL to send the GET request to.
+ */
+void httpsGETRequest(const char *url)
+{
   // create a WiFiClientSecure instance
   std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
 
@@ -59,33 +96,41 @@ void httpsGETRequest(const char* url) {
 
   // Initializing an HTTPS communication using the secure client
   Serial.print("[HTTPS] begin...\n");
-  if (https.begin(*client, url)) {  // HTTPS
+  if (https.begin(*client, url))
+  { // HTTPS
     Serial.print("[HTTPS] GET...\n");
 
     // start connection and send HTTP header
     int httpCode = https.GET();
 
     // httpCode will be negative on error
-    if (httpCode > 0) {
+    if (httpCode > 0)
+    {
       // HTTP header has been sent, and Server response header has been handled
       Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
 
       // file found at server
-      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+      {
         String payload = https.getString();
         Serial.println(payload);
       }
-    } else {
+    }
+    else
+    {
       Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
     }
 
     https.end();
-  } else {
+  }
+  else
+  {
     Serial.printf("[HTTPS] Unable to connect\n");
   }
 }
 
-void loop() {
+void loop()
+{
   // Example usage of the httpGETRequest function
   httpGETRequest("http://jsonplaceholder.typicode.com/users/1");
   delay(5000);
